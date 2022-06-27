3a54704322a041a5b019903a4716e1130246fb84
andy.lien <shaomin.lien@xrspace.io>
2022-06-27 16:07:49 +0800

    1. update axios settings

--- src/plugins/axios/axios.ts
+++ src/plugins/axios/axios.ts
@@ -1,16 +1,48 @@
-import axios from 'axios'
-import 'vite/client'
+import axios, {
+    AxiosRequestConfig, AxiosResponse 
+} from 'axios'
 
-const BASE_URL = import.meta.env.VITE_BASE_URL || 'https://api.lyrics.ovh/v1/'
-// requests settings
-const lyricsRequest = axios.create({
-    baseURL: BASE_URL
+const service = axios.create({
+    baseURL: `${import.meta.env.VITE_APP_BASE_DOMAIN}${import.meta.env.VITE_APP_BASE_API}`,
+    timeout: 10000,
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+    }
 })
 
-export const lyricsAPIs = {
-    getASongLyric: (data: Record<string, any>) => {
-        return lyricsRequest.get(`${data.artist}/${data.title}`)
+// request interceptor
+service.interceptors.request.use(
+    (config: AxiosRequestConfig) => {
+        if (!config.headers) {
+            throw new Error(
+                'Expected \'config\' and \'config.headers\' not to be undefined'
+            )
+        }
+        return config
+    },
+    error => {
+        console.log(error) // for debug
+        return Promise.reject(error)
     }
-}
+)
 
-export default axios
+// response interceptor
+service.interceptors.response.use(
+    (response: AxiosResponse) => {
+        const finalResponse = {
+            ...response,
+            target: response?.data?.data || {
+            }
+        }
+
+        return finalResponse
+    },
+    error => {
+        console.log('err' + error) // for debug
+        return Promise.reject({
+            ...error.response
+        })
+    }
+)
+
+export default service
